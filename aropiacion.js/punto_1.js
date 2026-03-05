4cda683f7c1f8199793f6a70989c94e4501ce748
jhonpoved01 <sjpp5506@gmail.com>
2026-03-05 16:59:35 -0500

    Elimino carpeta que no debía estar en main

--- aropiacion.js/punto_1.js
+++
@@ -1,6 +0,0 @@
-let base = parseFloat(prompt("Ingrese la base del terreno en metros:"));
-let altura = parseFloat(prompt("Ingrese la altura del terreno en metros:"));
-let area = base * altura;
-let perimetro = 2 * (base + altura);
-console.log("El área del terreno es: " + area + " metros cuadrados");
-console.log("El perímetro del terreno es: " + perimetro + " metros");
