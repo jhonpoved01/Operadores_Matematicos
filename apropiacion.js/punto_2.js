32bc535b7721432b9999c7527f1630ff17460558
jhonpoved01 <sjpp5506@gmail.com>
2026-03-05 17:02:35 -0500

    Elimino carpeta que no debía estar en desarrollo

--- apropiacion.js/punto_2.js
+++
@@ -1,5 +0,0 @@
-let nota1 = parseFloat(prompt("Ingrese la primera nota:"));
-let nota2 = parseFloat(prompt("Ingrese la segunda nota:"));
-let nota3 = parseFloat(prompt("Ingrese la tercera nota:"));
-let promedio = (nota1 + nota2 + nota3) / 3;
-console.log("El promedio del estudiante es: " + promedio);
